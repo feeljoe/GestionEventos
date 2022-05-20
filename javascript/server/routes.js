260187018e4e7cc87737b42a326769709d47da46
cr-ls <carlos.huerta013@gmail.com>
2022-05-20 00:22:42 -0700

    - Creación de páginas (index, event) - Instalación de nodemon y node js

--- javascript/server/routes.js
+++ javascript/server/routes.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 
-const alumno = require('../controllers/AlumnoController.js');
-const organizador = require('../controllers/OrganizadorController.js');
-const evento = require('../controllers/EventoController.js')
+const alumno = require("../controllers/AlumnoController.js");
+const organizador = require("../controllers/OrganizadorController.js");
+const evento = require("../controllers/EventoController.js");
 
 const router = express.Router();
 
 //Evento
-router.put('/evento/:_id', evento.actualizarEvento);
-router.get('/evento/:_id', evento.getEvento);
-router.get('/eventos', evento.getEventos);
-router.get('/registrosEvento/:_id', evento.getAlumnosPorEvento);
-router.delete('/evento/:_id', evento.eliminarEvento);
-router.post('/evento', evento.guardarEvento);
+router.put("/evento/:_id", evento.actualizarEvento);
+router.get("/evento/:_id", evento.getEvento);
+router.get("/eventos", evento.getEventos);
+router.get("/registrosEvento/:_id", evento.getAlumnosPorEvento);
+router.delete("/evento/:_id", evento.eliminarEvento);
+router.post("/evento", evento.guardarEvento);
 
 //Alumno
 // router.post('/alumno/:_id', alumno.actualizarAlumno);
@@ -23,6 +23,5 @@ router.post('/evento', evento.guardarEvento);
 // router.post('/organizador/:_id', organizador.actualizarOrganizador);
 // router.get('/organizador', organizador.getOrganizadores);
 // router.get('/organizador/:_id', organizador.getOrganizador);
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-
-const router = require('./javascript/server/routes.js');
-const {mongoose} = require('./javascript/database/Connection.js');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const router = require("./javascript/server/routes.js");
+const { mongoose } = require("./javascript/database/Connection.js");
 // const ErrorHandler = require('../errorHandler/errorHandler.js');
 
-
 const app = express();
-app.set('port', 3000);
+app.set("port", 3000);
 app.use(express.json());
-
-app.use(cors({ origin: '*' }));
-
-app.use('/api', router);
-
+app.use(cors({ origin: "*" }));
+app.use("/api", router);
 
 // obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
-var publicPath = path.resolve(__dirname, '../public'); //path.join(__dirname, 'public'); también puede ser una opción
+var publicPath = path.resolve(__dirname, "../public"); //path.join(__dirname, 'public'); también puede ser una opción
 
 // Para que los archivos estaticos queden disponibles.
 app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/frontend/html/index.html");
+  res.sendFile(__dirname + "/frontend/html/index.html");
 });
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("frontend"));
 
 app.listen(3000, () => {
   console.log("Servidor corriendo en el puerto " + 3000);
